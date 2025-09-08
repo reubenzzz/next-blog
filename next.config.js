f63c5df0aafd1f30baa7c7fc91e7bbbd2227938d
Reuben <90466947+reubenzzz@users.noreply.github.com>
2025-09-08 19:41:01 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: { domains: ['res.cloudinary.com', 'images.unsplash.com'] } // add domains you use
-}
-
-module.exports = nextConfig
